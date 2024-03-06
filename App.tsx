70d0c129bf8f50f7cad74c8dcde50ed05e47d7de
caisy2 <1823471207@qq.com>
2024-03-06 10:56:27 +0800

    初始配置

--- App.tsx
+++ App.tsx
@@ -6,54 +6,31 @@
  */
 
 import React from 'react';
+import {
+  Button,
+  CheckBox,
+  ButtonGroup,
+  Icon,
+  ListItem,
+  Avatar,
+  FAB,
+  Dialog
+} from '@rneui/themed';
 import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
+  Alert,
+  Stack,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -62,57 +39,160 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const [selectedIndex, setSelectedIndex] = React.useState([]);
+
+  const [visible, setVisible] = React.useState(true);
+
+  const [visible1, setVisible1] = React.useState(false);
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    // <SafeAreaView style={backgroundStyle}>
+    //   <StatusBar
+    //     barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+    //     backgroundColor={backgroundStyle.backgroundColor}
+    //   />
+    //   <ScrollView
+    //     contentInsetAdjustmentBehavior="automatic"
+    //     style={backgroundStyle}>
+    //     <Header />
+    //     <View
+    //       style={{
+    //         backgroundColor: isDarkMode ? Colors.black : Colors.white,
+    //       }}>
+    //       <Section title="Step One">
+    //         Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+    //         screen and then come back to see your edits.
+    //       </Section>
+    //       <Section title="See Your Changes">
+    //         <ReloadInstructions />
+    //       </Section>
+    //       <Section title="Debug">
+    //         <DebugInstructions />
+    //       </Section>
+    //       <Section title="Learn More">
+    //         Read the docs to discover what to do next:
+    //       </Section>
+    //       <LearnMoreLinks />
+    //     </View>
+    //   </ScrollView>
+    // </SafeAreaView>
+    <View>
+      <Text>hello world</Text>
+      <Button
+        color="secondary"
+        title="点击"
+        onPress={() => {
+          console.log('121212');
+        }}></Button>
+      <CheckBox checked title="Label" />
+      <CheckBox checked disabled title="Label" />
+      <ButtonGroup
+        selectMultiple
+        buttonStyle={{padding: 10}}
+        selectedButtonStyle={{backgroundColor: '#e2e2e2'}}
+        buttons={[
+          <Icon name="format-bold" />,
+          <Icon name="format-italic" />,
+          <Icon name="format-underline" />,
+        ]}
+        selectedIndexes={selectedIndex}
+        onPress={setSelectedIndex}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+
+      <ListItem bottomDivider>
+        <Avatar
+          rounded
+          source={{uri: 'https://randomuser.me/api/portraits/men/36.jpg'}}
+        />
+        <ListItem.Content>
+          <ListItem.Title>John Doe</ListItem.Title>
+          <ListItem.Subtitle>President</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+      <ListItem bottomDivider>
+        <Avatar
+          rounded
+          icon={{
+            name: 'person-outline',
+            type: 'material',
+            size: 26,
+          }}
+          containerStyle={{backgroundColor: '#c2c2c2'}}
+        />
+        <ListItem.Content>
+          <ListItem.Title>Alba King</ListItem.Title>
+          <ListItem.Subtitle>Vice President</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+      <ListItem>
+        <Avatar rounded title="A" containerStyle={{backgroundColor: 'grey'}} />
+        <ListItem.Content>
+          <ListItem.Title>Adam Eva</ListItem.Title>
+          <ListItem.Subtitle>Vice Chairman</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+
+      <View
+        style={{
+          alignItems: 'center',
+          paddingVertical: 5,
+          flexGrow: 1,
+        }}>
+        <Text style={{color: '#397af8', paddingVertical: 10}}>Small Size</Text>
+        <FAB
+          loading
+          visible={visible}
+          icon={{name: 'add', color: 'white'}}
+          size="small"
+        />
+        <Text style={{color: '#397af8', paddingVertical: 10}}>Large Size</Text>
+        <FAB
+          visible={visible}
+          icon={{name: 'add', color: 'white'}}
+          color="green"
+        />
+        <Text style={{color: '#397af8', paddingVertical: 10}}>
+          Primary Color
+        </Text>
+        <FAB
+          visible={visible}
+          title="Navigate"
+          upperCase
+          icon={{name: 'place', color: 'white'}}
+        />
+
+        <Text style={{color: '#397af8', paddingVertical: 10}}>Disabled</Text>
+
+        <FAB
+          visible={visible}
+          disabled
+          title="Extended"
+          icon={{
+            name: 'place',
+            color: 'white',
+          }}
+        />
+        <FAB
+          visible={visible}
+          onPress={() => setVisible(!visible)}
+          placement="right"
+          title="Hide"
+          icon={{name: 'delete', color: 'white'}}
+          color="red"
+        />
+        <FAB
+          visible={!visible}
+          onPress={() => setVisible(!visible)}
+          placement="left"
+          title="Show"
+          icon={{name: 'edit', color: 'white'}}
+          color="green"
+        />
+      </View>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
---
+++ component/flatList.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  useColorScheme,
+  Button,
+  View,
+  Alert,
+} from 'react-native';
+
+function flatList(): React.JSX.Element {
+    return (
+        <View>
+            
+        </View>
+    )
+}
